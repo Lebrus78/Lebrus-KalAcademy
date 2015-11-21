fd3a779445aa3a63a3428605224f65d7ff5da848
Lebrus78 <autoridadno@gmail.com>
2015-11-21 14:17:35 -0800

    initial code

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    /// <summary>
+    /// Provides a space to create a command app for a bank, BedrockBank
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the customer
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Customer's account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Balance of the account
+        /// </summary>
+        public decimal Balance { get; set; }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
